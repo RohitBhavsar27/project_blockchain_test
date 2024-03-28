d36f3321db8c9b1cd390acdb84e643ffe37f0da1
Rohit Mahendra Bhavsar <130300699+RohitBhavsar27@users.noreply.github.com>
2024-03-28 19:58:22 +0530

    hardhat configuration for sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,20 @@
 require("@nomicfoundation/hardhat-toolbox");
+
+const ALCHEMY_API_KEY = "-TEuj7vqiCkgfVpgpnuoVRJwhMfSMQQb";
+const SEPOLIA_PRIVATE_KEY = "823ca69b734d4f91ffaecb6fd98d0fab0825e3560f418c726a340e854e15861c";
+
 module.exports = {
-  solidity: "0.8.9",
   networks: {
     hardhat: {
-      chainId: 1337,
     },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    }
+  },
+  solidity: "0.8.9",
+  etherscan: {
+    apikey: "C24CWQCGW8ZWRFXP9ECIURPDBIQTNVRB42",
   },
   paths: {
     artifacts: "./client/src/artifacts",
