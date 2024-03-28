a98acfef187a5911c2e3c0f154a363f819776e6f
Rohit Mahendra Bhavsar <130300699+RohitBhavsar27@users.noreply.github.com>
2024-03-28 20:09:47 +0530

    added another sepolia account

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 const ALCHEMY_API_KEY = "-TEuj7vqiCkgfVpgpnuoVRJwhMfSMQQb";
-const SEPOLIA_PRIVATE_KEY = "823ca69b734d4f91ffaecb6fd98d0fab0825e3560f418c726a340e854e15861c";
+const SEPOLIA_PRIVATE_KEY_1 = "823ca69b734d4f91ffaecb6fd98d0fab0825e3560f418c726a340e854e15861c";
+const SEPOLIA_PRIVATE_KEY_2 = "2acf65a68992ddc0102ceea6125d35be8c06b574a3e5d62c8425f56e5e975de4";
 
 module.exports = {
   networks: {
@@ -9,7 +10,7 @@ module.exports = {
     },
     sepolia: {
       url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [SEPOLIA_PRIVATE_KEY],
+      accounts: [SEPOLIA_PRIVATE_KEY_1, SEPOLIA_PRIVATE_KEY_2],
     }
   },
   solidity: "0.8.9",
